3c43fe8ecb1aec0d994f4f6785c8c9cefbe2ddd7
Nugrahanto <rianirsyahnugrahanto@gmail.com>
2017-03-23 00:12:58 +0700

    FirstCommit

---
+++ Assets/Pausebtn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausebtn : MonoBehaviour {
+
+	//bool Pause = false;
+	public GameObject btnPause;
+	public GameObject btnPlay;
+	public bool paused;
+
+	// Use this for initialization
+	void Start () {
+		paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Pause () {
+
+		paused = !paused;
+
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+				btnPause.SetActive (false);
+				btnPlay.SetActive (true);
+			}
+			else
+			{
+				Time.timeScale = 1;
+				btnPause.SetActive (true);
+				btnPlay.SetActive (false);
+			}
+	}  
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D myRigidBody;
+	private Animator myAnim;
+	public float playerJumpForce = 500f;
+	private float playerHurtTime = -1;
+	private Collider2D myCollider;
+	public Text scoreText;
+	private float startTime;
+	private int jumpsLeft = 2;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
+		myCollider = GetComponent<Collider2D> ();	
+
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.LoadLevel ("Title");
+		}
+
+		if (playerHurtTime == -1) {
+
+			if ((Input.GetButtonUp ("Jump") || Input.GetButtonUp("Fire1")) && jumpsLeft > 0) {
+
+				if (myRigidBody.velocity.y < 0) {
+					myRigidBody.velocity = Vector2.zero;
+				}
+
+				if (jumpsLeft == 1) {
+					myRigidBody.AddForce (transform.up * playerJumpForce * 0.75f);
+				} else {
+					myRigidBody.AddForce (transform.up * playerJumpForce);
+				}
+			
+				jumpsLeft--;
+
+			}
+			myAnim.SetFloat ("vVelocity", myRigidBody.velocity.y);
+			scoreText.text = (Time.time - startTime).ToString ("0.0");
+		} 
+		else {
+			if (Time.time > playerHurtTime + 2) {
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if (collision.collider.gameObject.layer == LayerMask.NameToLayer ("Enemy")) {
+
+			foreach (RintanganSpawner spawner in FindObjectsOfType<RintanganSpawner>()) {
+				spawner.enabled = false;
+			}
+
+			foreach (MoveLeft moveLefter in FindObjectsOfType<MoveLeft>()) {
+				moveLefter.enabled = false;
+			}
+
+			playerHurtTime = Time.time;
+			myAnim.SetBool ("playerHurt", true);
+			myRigidBody.velocity = Vector2.zero;
+			myRigidBody.AddForce (transform.up * playerJumpForce);
+			myCollider.enabled = false;
+		} 
+		else if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground")){
+			jumpsLeft = 2;
+		}
+	}
+}
