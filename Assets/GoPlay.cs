742aa6c12402ae357432ac7e1b59c79f1709bdbc
Nugrahanto <rianirsyahnugrahanto@gmail.com>
2017-03-30 08:11:12 +0700

    FixBugs

--- Assets/GoPlay.cs
+++ Assets/GoPlay.cs
@@ -13,10 +13,12 @@ public class GoPlay : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			SceneManager.LoadScene("Title");
+		}
 	}
 		
 	public void goPlay(){
-		SceneManager.LoadScene("Title");	
+		SceneManager.LoadScene("Title");
 	}
 }
